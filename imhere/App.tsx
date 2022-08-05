c6659d6f1ccec734e756504f794b86e2f02872f6
Rogério Tavares <rogerioctf@gmail.com>
2022-08-05 06:44:56 -0300

    Utilizando StyleSheet

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,22 +1,28 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "#131016",
-      padding: 24
-    }}>
-      <Text style={{
-        color: "#fdfcfe",
-        fontSize: 24,
-        fontWeight: "bold",
-        marginTop: 48
-        }}>Nome do evento</Text>
-      <Text style={{
-        color: "#6b6b6b",
-        fontSize: 16
-      }}>Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>21/08/2022</Text>
     </ View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16
+  }
+})
