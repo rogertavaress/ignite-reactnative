56a999a1f266740b5e2b9ed0b88dda610ada365f
Rogério Tavares <rogerioctf@gmail.com>
2022-08-05 07:05:01 -0300

    Botão de Adicionar

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import {styles} from "./styles"
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("click");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
@@ -11,6 +16,9 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6b6b6b"
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </ View>
   )
 }
