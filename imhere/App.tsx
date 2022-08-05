fc573f7a4e145afd765ac5a9e9afed42f86890fb
Rogério Tavares <rogerioctf@gmail.com>
2022-08-05 06:37:50 -0300

    Estilizando Elementos

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,10 +1,22 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <>
-      <Text>React</Text>
-      <Text>Native</Text>
-    </>
+    <View style={{
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24
+    }}>
+      <Text style={{
+        color: "#fdfcfe",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 48
+        }}>Nome do evento</Text>
+      <Text style={{
+        color: "#6b6b6b",
+        fontSize: 16
+      }}>Native</Text>
+    </ View>
   )
 }
